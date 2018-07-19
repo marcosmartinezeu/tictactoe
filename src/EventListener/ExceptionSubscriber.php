cd9732083ee71f783771567bd1f6a3efb49e630f
mmartinez <mmartinez@dinahosting.com>
2018-07-19 12:07:55 +0200

    Add error response

---
+++ src/EventListener/ExceptionSubscriber.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+    private $httpErrorCodes = [500, 404, 400, 412];
+
+    /**
+     * @param GetResponseForExceptionEvent $event
+     */
+    public function onKernelException(GetResponseForExceptionEvent $event): void
+    {
+        $exception = $event->getException();
+
+        $exceptionData = [
+            'error' => true,
+            'message' => $exception->getMessage()
+        ];
+
+        $statusCode = !in_array($exception->getCode(), $this->httpErrorCodes) ? 500 : $exception->getCode();
+
+        $response = new JsonResponse($exceptionData, $statusCode);
+        $event->setResponse($response);
+    }
+
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::EXCEPTION => 'onKernelException',
+        ];
+    }
+}
